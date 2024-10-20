070c953f3023df49599b63e7985446d50df9b315
andresguepar <34109816+andresguepar@users.noreply.github.com>
2024-10-20 12:49:08 -0500

    Si

---
+++ src/main/java/co/edu/unihumboldt/to_do_list/controllers/TaskController.java
@@ -0,0 +1,5 @@
+package co.edu.unihumboldt.to_do_list.controllers;
+
+public class TaskController {
+
+}
--- src/main/java/co/edu/unihumboldt/to_do_list/mapping/dtos/TaskDto.java
+++ src/main/java/co/edu/unihumboldt/to_do_list/mapping/dtos/TaskDto.java
@@ -1,12 +1,7 @@
 package co.edu.unihumboldt.to_do_list.mapping.dtos;
 
 import co.edu.unihumboldt.to_do_list.domain.enums.Priority;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 @Builder
