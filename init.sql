4d1a6076fcd8ccb9a8b4cb9dc4eff34c68802057
andresguepar <34109816+andresguepar@users.noreply.github.com>
2024-11-11 02:05:21 -0500

    Test Actions

--- init.sql
+++ init.sql
@@ -11,10 +11,10 @@ CREATE TABLE IF NOT EXISTS tasks (
     );
 
 INSERT INTO tasks (completed,completed_date,creation_date,description,limit_date,priority,reward_level,title) VALUES
-                                                                                                                  (0,NULL,'2024-10-20','Paginas 12 ','2024-10-22','HIGH',1,'Leer Libro'),
+                                                                                                                  (0,'','2024-10-20','Paginas 12 ','2024-10-22','HIGH',1,'Leer Libro'),
                                                                                                                   (1,'2024-10-20','2024-10-20','Paginas 11222 ','2024-10-22','HIGH',2,'Hacer Tarea'),
-                                                                                                                  (0,NULL,NULL,'','2024-10-21','MEDIUM',2,'Something'),
+                                                                                                                  (0,'','','','2024-10-21','MEDIUM',2,'Something'),
                                                                                                                   (1,'2024-10-23','2024-10-23','ssss','2024-10-24','MEDIUM',1,'Tas'),
-                                                                                                                  (1,NULL,'2024-10-23','Si','2024-10-30','MEDIUM',4,'Estudiar '),
-                                                                                                                  (0,NULL,'2024-10-23','Hola','2024-10-30','MEDIUM',1,'Hola'),
-                                                                                                                  (0,NULL,'2024-10-23','','2024-10-24','HIGH',1,'Si');
+                                                                                                                  (1,'','2024-10-23','Si','2024-10-30','MEDIUM',4,'Estudiar '),
+                                                                                                                  (0,'','2024-10-23','Hola','2024-10-30','MEDIUM',1,'Hola'),
+                                                                                                                  (0,'','2024-10-23','','2024-10-24','HIGH',1,'Si');
