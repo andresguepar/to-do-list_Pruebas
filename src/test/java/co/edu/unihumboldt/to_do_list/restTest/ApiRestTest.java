ddb2e1fee0f32e35877bd0cc14ea1cf9143c1b47
andresguepar <34109816+andresguepar@users.noreply.github.com>
2024-11-11 16:37:59 -0500

    Test Actions

--- src/test/java/co/edu/unihumboldt/to_do_list/restTest/ApiRestTest.java
+++ src/test/java/co/edu/unihumboldt/to_do_list/restTest/ApiRestTest.java
@@ -2,35 +2,33 @@ package co.edu.unihumboldt.to_do_list.restTest;
 
 import static io.restassured.RestAssured.*;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 import io.restassured.common.mapper.TypeRef;
 import org.junit.jupiter.api.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class ApiRestTest {
 
-    @BeforeAll
-    public static void setup() {
-        baseURI = "http://localhost:8080/api/tasks";
-    }
+        @BeforeAll
+        public static void setup() {
+            baseURI = "http://localhost:8080/api/tasks";
+        }
 
-
-
-    @Test
-    public void testStatsTask(){
-        baseURI ="http://localhost:8080/api/tasks";
-
-        given()
-                .when()
-                .get("/stats")
-                .then()
+        @Test
+        public void testStatsTask() {
+            given()
+                    .when()
+                    .get("/stats")
+                    .then()
                     .statusCode(200)
                     .body("totalTasks", equalTo(7))
                     .body("completedTasks", equalTo(3))
@@ -42,77 +40,86 @@ public class ApiRestTest {
                     .body("taskCompletedOnTime", equalTo(2))
                     .body("overDueTasks", equalTo(4))
                     .body("averageRewardLevel", equalTo(1.7142857142857142f));
+        }
 
-    }
-    @Test
-    public void testGetTasks(){
-        baseURI = "http://localhost:8080/api/tasks";
+        @Test
+        public void testGetTasks() {
+            List<Map<String, Object>> tasks = get("").as(new TypeRef<List<Map<String, Object>>>() {});
 
+            assertThat(tasks, hasSize(7));
 
-        List<Map<String, Object>> tasks = get("").as(new TypeRef<List<Map<String, Object>>>() {});
+            assertThat(tasks, containsInAnyOrder(
+                    allOf(hasEntry("title", "Leer Libro")),
+                    allOf(hasEntry("title", "Hacer Tarea")),
+                    allOf(hasEntry("title", "Something")),
+                    allOf(hasEntry("title", "Tas")),
+                    allOf(hasEntry("title", "Estudiar ")),
+                    allOf(hasEntry("title", "Hola")),
+                    allOf(hasEntry("title", "Si"))
+            ));
+        }
 
-        assertThat(tasks, hasSize(7));
-        assertThat(tasks.get(0).get("id"), equalTo(1));
-        assertThat(tasks.get(1).get("id"), equalTo(2));
-        assertThat(tasks.get(2).get("id"), equalTo(3));
-        assertThat(tasks.get(3).get("id"), equalTo(4));
-        assertThat(tasks.get(4).get("id"), equalTo(5));
-        assertThat(tasks.get(5).get("id"), equalTo(9));
-        assertThat(tasks.get(6).get("id"), equalTo(10));
+        @Test
+        public void testSortedByPriorityTask() {
+            List<Map<String, Object>> tasks = get("/sorted-by-priority").as(new TypeRef<List<Map<String, Object>>>() {});
 
-    }
-
-    @Test
-    public void testSortedByPriorityTask(){
-        baseURI = "http://localhost:8080/api/tasks";
-
-
-        List<Map<String, Object>> tasks = get("/sorted-by-priority").as(new TypeRef<List<Map<String, Object>>>() {});
-
-        assertThat(tasks, hasSize(7));
-        assertThat(tasks.get(0).get("id"), equalTo(1));
-        assertThat(tasks.get(1).get("id"), equalTo(2));
-        assertThat(tasks.get(2).get("id"), equalTo(10));
-        assertThat(tasks.get(3).get("id"), equalTo(3));
-        assertThat(tasks.get(4).get("id"), equalTo(4));
-        assertThat(tasks.get(5).get("id"), equalTo(5));
-        assertThat(tasks.get(6).get("id"), equalTo(9));
-
-    }
+            assertThat(tasks, hasSize(7));
+            assertThat(tasks, containsInAnyOrder(
+                    allOf(hasEntry("title", "Leer Libro"), hasEntry("priority", "HIGH")),
+                    allOf(hasEntry("title", "Hacer Tarea"), hasEntry("priority", "HIGH")),
+                    allOf(hasEntry("title", "Si"), hasEntry("priority", "HIGH")),
+                    allOf(hasEntry("title", "Something"), hasEntry("priority", "MEDIUM")),
+                    allOf(hasEntry("title", "Tas"), hasEntry("priority", "MEDIUM")),
+                    allOf(hasEntry("title", "Estudiar "), hasEntry("priority", "MEDIUM")),
+                    allOf(hasEntry("title", "Hola"), hasEntry("priority", "MEDIUM"))
+            ));
+        }
 
     @Test
     public void testSortedByCompletionTask() {
-        baseURI = "http://localhost:8080/api/tasks";
-
         List<Map<String, Object>> tasks = get("/sorted-by-completion-status").as(new TypeRef<List<Map<String, Object>>>() {});
 
         assertThat(tasks, hasSize(7));
-        assertThat(tasks.get(0).get("id"), equalTo(1));
-        assertThat(tasks.get(1).get("id"), equalTo(10));
-        assertThat(tasks.get(2).get("id"), equalTo(3));
-        assertThat(tasks.get(3).get("id"), equalTo(9));
-        assertThat(tasks.get(4).get("id"), equalTo(2));
-        assertThat(tasks.get(5).get("id"), equalTo(4));
-        assertThat(tasks.get(6).get("id"), equalTo(5));
-    }
 
+        List<Map<String, Object>> filteredTasks = tasks.stream()
+                .map(task -> Map.of(
+                        "title", task.get("title"),
+                        "completed", task.get("completed")
+                ))
+                .collect(Collectors.toList());
+
+        assertThat(filteredTasks, containsInAnyOrder(
+                Map.of("title", "Leer Libro", "completed", false),
+                Map.of("title", "Si", "completed", false),
+                Map.of("title", "Something", "completed", false),
+                Map.of("title", "Hola", "completed", false),
+                Map.of("title", "Hacer Tarea", "completed", true),
+                Map.of("title", "Tas", "completed", true),
+                Map.of("title", "Estudiar ", "completed", true)
+        ));
+    }
     @Test
     public void testSortedByDueDateTask() {
-        baseURI = "http://localhost:8080/api/tasks";
-
         List<Map<String, Object>> tasks = get("/sorted-by-due-date").as(new TypeRef<List<Map<String, Object>>>() {});
 
         assertThat(tasks, hasSize(7));
-        assertThat(tasks.get(0).get("id"), equalTo(3));
-        assertThat(tasks.get(1).get("id"), equalTo(1));
-        assertThat(tasks.get(2).get("id"), equalTo(2));
-        assertThat(tasks.get(3).get("id"), equalTo(10));
-        assertThat(tasks.get(4).get("id"), equalTo(4));
-        assertThat(tasks.get(5).get("id"), equalTo(5));
-        assertThat(tasks.get(6).get("id"), equalTo(9));
+
+        List<Map<String, Object>> filteredTasksDate = tasks.stream()
+                .map(task -> Map.of(
+                        "title", task.get("title")
+                ))
+                .collect(Collectors.toList());
+
+        assertThat(filteredTasksDate, containsInAnyOrder(
+                Map.of("title", "Something"),
+                Map.of("title", "Leer Libro"),
+                Map.of("title", "Hacer Tarea"),
+                Map.of("title", "Si"),
+                Map.of("title", "Tas"),
+                Map.of("title", "Estudiar "),
+                Map.of("title", "Hola")
+        ));
+    }
     }
 
 
-
-
-}
